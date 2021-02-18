ed7f0527f32dfae59f88280588ed85d39dd3f32d
thediidu <79207774+thediidu@users.noreply.github.com>
2021-02-18 15:57:38 +0200

    Update game.test.js
    is this is? failing tests?

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -22,7 +22,7 @@ describe('Game', () => {
   describe('Game', () => {
     it('Initializes with two players', async () => {
       expect(game.p1).toBe('Salem')
-      expect(game.p2).toBe('Nate')
+      expect(game.p2).toBe('Bananas')
     })
 
     it('Initializes with an empty board', async () => {
@@ -38,7 +38,7 @@ describe('Game', () => {
       expect(new Game(p1, p2).player).toBe('Salem')
 
       Math.random = () => 0.6
-      expect(new Game(p1, p2).player).toBe('Nate')
+      expect(new Game(p1, p2).player).toBe('Bananas')
     })
   })
 
@@ -60,7 +60,7 @@ describe('Game', () => {
       const game = new Game(p1, p2)
       expect(game.player).toBe('Salem')
       game.nextPlayer()
-      expect(game.player).toBe('Nate')
+      expect(game.player).toBe('Bananas')
     })
   })
 
